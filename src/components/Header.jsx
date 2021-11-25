33c7f2bf8fb69ba64000a88fb90ce25c267c746d
cynthiaswill <yzhang4@msn.com>
2021-11-25 02:56:16 +0000

    changed header color n tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="header">
-      <h1 id="header">
-        <Link to="/">Board Game Reviews</Link>
+    <header>
+      <h1 className="header">
+        <Link id="header" to="/">
+          Board Game Reviews
+        </Link>
       </h1>
     </header>
   );
