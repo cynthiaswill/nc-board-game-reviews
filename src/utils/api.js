e12549995095f49172131b3a8983e7ce450c0a05
cynthiaswill <yzhang4@msn.com>
2021-11-26 14:27:51 +0000

    edit comment request function

--- src/utils/api.js
+++ src/utils/api.js
@@ -58,4 +58,6 @@ export const incLikes = (comment_id, { inc_votes }) => {
   return gamesApi.patch(`/comments/${comment_id}`, { inc_votes });
 };
 
-export const EditComment = () => {};
+export const EditComment = (comment_id, { body }) => {
+  return gamesApi.patch(`/comments/${comment_id}`, { body });
+};
