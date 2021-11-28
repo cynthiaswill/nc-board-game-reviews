c472b821ae77b34d429a38c61d5150112ad0b344
cynthiaswill <yzhang4@msn.com>
2021-11-28 14:12:19 +0000

    login page styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+import "../styles/Login.css";
 import { useEffect, useState } from "react";
 import { getUsers } from "../utils/api";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,34 +14,35 @@ export default function Login({ setUser }) {
   }, []);
 
   return (
-    <div className="users-container">
-      {users.map((user) => {
-        return (
-          <div key={user.username} className="user-card">
-            <div id="user-tag">
-              <img
-                src="https://source.unsplash.com/random/300x200"
-                alt={user.username}
-                className="user-pic"
-              ></img>
+    <div className="login-container">
+      <h3 className="login-title">Please choose an account to log in:</h3>
+      <section className="users-container">
+        {users.map((user) => {
+          return (
+            <div key={user.username} className="user-card">
+              <div>
+                <img
+                  src="https://source.unsplash.com/random/300x200"
+                  alt={user.username}
+                  className="user-pic"
+                ></img>
+                <br />
+                <Link to={`/users/${user.username}`}>{user.username}</Link>
+              </div>
               <br />
-              <Link id="user-tag-link" to={`/users/${user.username}`}>
-                {user.username}
-              </Link>
+              <button
+                className="user-login-button"
+                onClick={() => {
+                  setUser(user);
+                  navigate("/reviews");
+                }}
+              >
+                Login as {user.username}
+              </button>
             </div>
-            <br />
-            <button
-              className="user-login-button"
-              onClick={() => {
-                setUser(user);
-                navigate("/reviews");
-              }}
-            >
-              Login as {user.username}
-            </button>
-          </div>
-        );
-      })}
+          );
+        })}
+      </section>
     </div>
   );
 }
