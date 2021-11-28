6465ff78a03eb76a29b33844679dc5a2c3ba95dc
cynthiaswill <yzhang4@msn.com>
2021-11-28 02:43:13 +0000

    compose review

--- src/components/Compose.jsx
+++ src/components/Compose.jsx
@@ -32,36 +32,37 @@ export default function Compose({ categories, setCategories }) {
 
   return (
     <div>
-      <main className="main">
-        <h3 className="compose-title">Compose a new Review:</h3>
-        <p>
-          Do you need to create a new category?
-          <button
-            onClick={() => {
-              setNeedNewCat(true);
-            }}
-          >
-            Yes
-          </button>
-          <button
-            onClick={() => {
-              setNeedNewCat(false);
-            }}
-          >
-            No
-          </button>
-        </p>
-        {needNewCat ? (
-          <>
+      <main className="main-compose">
+        <div className="compose-container">
+          <h3 className="compose-title">Compose a new Review:</h3>
+          <p className="yes-or-no">
+            Do you need to create a new category?
+            <button
+              onClick={() => {
+                setNeedNewCat(true);
+              }}
+            >
+              Yes
+            </button>
+            <button
+              onClick={() => {
+                setNeedNewCat(false);
+              }}
+            >
+              No
+            </button>
+          </p>
+          {needNewCat ? (
             <section className="yes-or-no">
               {categories
                 .map((category) => {
                   return category.slug;
                 })
                 .includes(newCategory.slug) ? null : (
-                <form id="newCategorySubmit" onSubmit={submitCategory}>
+                <form onSubmit={submitCategory}>
                   <input
                     type="text"
+                    size="22"
                     placeholder="enter new category name"
                     name="slug"
                     onChange={(event) => {
@@ -75,6 +76,7 @@ export default function Compose({ categories, setCategories }) {
                   />
                   <input
                     type="text"
+                    size="22"
                     placeholder="please give a description..."
                     name="description"
                     onChange={(event) => {
@@ -90,44 +92,78 @@ export default function Compose({ categories, setCategories }) {
                 </form>
               )}
             </section>
-          </>
-        ) : (
-          <form className="compose-form">
-            <p>Please give a review title:</p>
-            <input
-              type="text"
-              placeholder="review title"
-              name="title"
-              onChange={(event) => {
-                setNewReview((current) => {
-                  return {
-                    ...current,
-                    title: event.target.value,
-                  };
-                });
-              }}
-            />
-            <p>Please choose your category:</p>
-            <select
-              name="category"
-              id="category-chooser"
-              onChange={(event) => {
-                setNewCategory((current) => {
-                  return { ...current, slug: event.target.value };
-                });
-              }}
-            >
-              {categories.map((category) => {
-                return (
-                  <option
-                    key={category.slug}
-                    value={category.slug}
-                  >{`${category.slug}`}</option>
-                );
-              })}
-            </select>
-          </form>
-        )}
+          ) : (
+            <form className="compose-form">
+              <p>Please give a review title:</p>
+              <input
+                type="text"
+                placeholder="review title"
+                name="title"
+                onChange={(event) => {
+                  setNewReview((current) => {
+                    return {
+                      ...current,
+                      title: event.target.value,
+                    };
+                  });
+                }}
+              />
+              <br />
+              <p>Please choose your category:</p>
+              <select
+                name="category"
+                onChange={(event) => {
+                  setNewReview((current) => {
+                    return { ...current, category: event.target.value };
+                  });
+                }}
+              >
+                {categories.map((category) => {
+                  return (
+                    <option
+                      key={category.slug}
+                      value={category.slug}
+                    >{`${category.slug}`}</option>
+                  );
+                })}
+              </select>
+              <br />
+              <p>Please enter game designer's name:</p>
+              <input
+                type="text"
+                placeholder="designer"
+                name="designer"
+                onChange={(event) => {
+                  setNewReview((current) => {
+                    return {
+                      ...current,
+                      title: event.target.value,
+                    };
+                  });
+                }}
+              />
+              <br />
+              <p>Please write review in the box below:</p>
+              <textarea
+                id="review-writing-box"
+                rows="20"
+                placeholder="write your review here ..."
+                name="review_body"
+                onChange={(event) => {
+                  setNewReview((current) => {
+                    return {
+                      ...current,
+                      title: event.target.value,
+                    };
+                  });
+                }}
+              />
+              <br />
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        </div>
       </main>
     </div>
   );
