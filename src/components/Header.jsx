c5e81f06db4f7da25c38a707e051070c094d55ed
cynthiaswill <yzhang4@msn.com>
2021-12-30 22:16:17 +0000

    fixs reset selectors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,13 @@ export default function Header() {
   return (
     <header>
       <h1 className="header">
-        <Link id="header" to="/">
+        <Link
+          id="header"
+          to="/"
+          onClick={() => {
+            window.location.reload(false);
+          }}
+        >
           Board Game Reviews
         </Link>
       </h1>
