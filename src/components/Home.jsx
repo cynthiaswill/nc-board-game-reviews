8cda0b3559edc6e756d94c4982ba509a464df164
cynthiaswill <yzhang4@msn.com>
2022-01-01 03:28:03 +0000

    counts timeout to redirect from home to reviews

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,37 @@
+import { Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [redirect, setRedirect] = useState(false);
+  const [counter, setCounter] = useState(3);
+
+  useEffect(() => {
+    const counter = setInterval(() => {
+      setCounter((current) => {
+        return current - 1;
+      });
+    }, 1000);
+
+    const timer = setTimeout(() => {
+      setRedirect(true);
+    }, 3000);
+
+    return () => {
+      clearInterval(counter);
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="welcome">
-      <h2>Welcome to NC Board Game Reviews</h2>
-    </div>
+    <>
+      {redirect ? (
+        <Navigate to="/reviews" />
+      ) : (
+        <div className="welcome">
+          <h2>Welcome to NC Board Game Reviews</h2>
+          <p>redirecting after {counter} seconds ...</p>
+        </div>
+      )}
+    </>
   );
 }
