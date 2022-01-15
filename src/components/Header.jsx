1566a24263c3655ca7cf731fe78ac641ed8a3cb6
cynthiaswill <yzhang4@msn.com>
2022-01-15 13:15:17 +0000

    adds tilt effect to title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
+import Tilt from "react-parallax-tilt";
 
 export default function Header({ setReset }) {
   const navigate = useNavigate();
@@ -15,20 +16,22 @@ export default function Header({ setReset }) {
             setReset(true);
           }}
         >
-          <div
-            style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
-          >
+          <Tilt perspective={500} scale={1.05}>
             <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
+              style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
             >
-              <FaHome style={{ paddingBottom: "5px", alignSelf: "center" }} />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <FaHome style={{ paddingBottom: "5px", alignSelf: "center" }} />
+              </div>
+              <div id="header-text">Board Game Reviews</div>
             </div>
-            <div id="header-text">Board Game Reviews</div>
-          </div>
+          </Tilt>
         </Link>
       </h1>
     </header>
