30cadc7b5c0db760ee35ee45a9d2b46db47d8b52
cynthiaswill <yzhang4@msn.com>
2022-02-01 03:18:44 +0000

    move getOnlineStatus into chat window click

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,11 +3,20 @@ import { useLocation } from "react-router-dom";
 import { ChatContext } from "../contexts/ChatContext";
 import ChatWindow from "./ChatWindow";
 import useWindowDimensions from "../hooks/WindowDimentions";
+import { getOnlineUsers } from "../utils/api";
+import { OnlineUsersContext } from "../contexts/OnlineUsersContext";
 
 export default function Chat() {
   const { isChatOpen, setIsChatOpen } = useContext(ChatContext);
+  const { setOnlineUsers } = useContext(OnlineUsersContext);
   const location = useLocation();
   const { width } = useWindowDimensions();
+
+  const getOnlineStatus = () => {
+    getOnlineUsers().then(({ data }) => {
+      setOnlineUsers([...data.list.onlineUsers]);
+    });
+  };
 
   return (
     <>
@@ -26,6 +35,7 @@ export default function Chat() {
               }
               onClick={() => {
                 setIsChatOpen(true);
+                getOnlineStatus();
               }}
             >
               <i className="far fa-comments" /> Live Chat
