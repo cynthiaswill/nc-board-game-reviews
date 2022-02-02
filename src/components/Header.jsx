8d08e78c2a94def72fbc0aa5f48257d9a732f0a6
cynthiaswill <yzhang4@msn.com>
2022-02-02 12:33:38 +0000

    remove unused

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import Tilt from "react-parallax-tilt";
