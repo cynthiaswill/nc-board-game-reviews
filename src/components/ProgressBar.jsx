eefeaabe0379bdfa0bdbc1944bb4b1c5ef6a1fb1
cynthiaswill <yzhang4@msn.com>
2022-02-05 00:55:03 +0000

    sets different loading speed by pages

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function ProgressBar() {
+  const path = useLocation().pathname;
   const [progress, updateProgress] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      updateProgress((prev) => {
-        const updatedProgress = prev + 10;
+    const interval = setInterval(
+      () => {
+        updateProgress((prev) => {
+          const updatedProgress = prev + 10;
 
-        if (updatedProgress === 100) {
-          clearInterval(interval);
-        }
-        return updatedProgress;
-      });
-    }, 200);
-  }, []);
+          if (updatedProgress === 100) {
+            clearInterval(interval);
+          }
+          return updatedProgress;
+        });
+      },
+      path === "/" ? 200 : 50
+    );
+  }, [path]);
 
   return (
     <>
